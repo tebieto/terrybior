5a5281c509dfea96db307a100ec6e4cb1d72df02
Terry Ebieto <tebieto@gmail.com>
2020-04-01 11:46:07 +0100

    Addedand Styled CollectionPreview functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx';
+import SHopPage from './pages/shop/shop.component.jsx'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={SHopPage} />
         <Route exact path='*' component={HomePage} />
       </Switch>
     </div>
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import SHOP_DATA from './shop.data';
+
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+
+    render() {
+        const { collections } = this.state;
+        return (
+            <div>
+                {
+                    collections.map(({ id, ...otherProps }) => (
+                        <CollectionPreview key={id} {...otherProps} />
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default ShopPage;
