6a0309a90ae424b1a82801530692656ccd3879a4
Terry Ebieto <tebieto@gmail.com>
2020-04-02 15:42:38 +0100

    AddedFirebase Authentication, SignIn With Google

--- src/App.js
+++ src/App.js
@@ -7,18 +7,45 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import SHopPage from './pages/shop/shop.component.jsx'
 import Header from './components/header/header.component'
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={SHopPage} />
-        <Route exact path='*' component={HomePage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFriomAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFriomAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFriomAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={SHopPage} />
+          <Route exact path='/signin' component={SignInAndSignUp} />
+          <Route exact path='*' component={HomePage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-//import {ReactComponent as Logo} from '../../assets/crown.svg';
+import { auth } from '../../firebase/firebase.utils';
+
+import {ReactComponent as Logo} from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = () => (
+const Header = ({ currentUser }) => (
+
     <div className='header'>
         <Link className='logo-container' to='/'>
-           <div className='logo'>LOGO</div> {/* <Logo className='logo' /> */}
+            <Logo className='logo' />
         </Link>
         <div className='options'>
             <Link className='option' to='/shop'>SHOP</Link>
             <Link className='option' to='/contact'>CONTACT</Link>
+            {
+                currentUser ?
+                <div className='option' onClick={() => auth.signOut()}>
+                    SIGN OUT
+                </div>
+                :
+                <Link to='/signin'>SIGN IN</Link>
+            }
         </div>
     </div>
 );
