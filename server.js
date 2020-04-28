da0379e88d1a961146f45754dbf0374150565349
Terry Ebieto <tebieto@gmail.com>
2020-04-28 15:35:54 +0100

    extended backend config

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('cors');
+const path = require('path');
+
+
+if (process.env.NODE_ENV !=== 'production') require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
