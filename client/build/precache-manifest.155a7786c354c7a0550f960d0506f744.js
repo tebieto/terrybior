4b82c40daaf6656ff8bafa712a8ceb957ea6754d
Terry Ebieto <tebieto@gmail.com>
2020-05-04 14:09:48 +0100

    removed unwanted images

---
+++ client/build/precache-manifest.155a7786c354c7a0550f960d0506f744.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697d0676364b60c717d4329952aa26b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39b4fd00a6e6faf6316a",
+    "url": "/static/css/4.188bd450.chunk.css"
+  },
+  {
+    "revision": "bf2648b2679a76bb0bc9",
+    "url": "/static/css/5.7bffbeb8.chunk.css"
+  },
+  {
+    "revision": "da917afb8204780ca2f9",
+    "url": "/static/css/6.f60f2b47.chunk.css"
+  },
+  {
+    "revision": "33438d935309003dd8cd",
+    "url": "/static/css/7.544f3a49.chunk.css"
+  },
+  {
+    "revision": "c5f0809fda00382efc34",
+    "url": "/static/css/8.b98eb2ac.chunk.css"
+  },
+  {
+    "revision": "70942ef21a8b7dbb8fdd",
+    "url": "/static/css/main.a0e759b8.chunk.css"
+  },
+  {
+    "revision": "8d50d9ea15e2341e9c18",
+    "url": "/static/js/2.e244a1c9.chunk.js"
+  },
+  {
+    "revision": "ad61e5a3703d8af976e070e9896b739a",
+    "url": "/static/js/2.e244a1c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f021cdfdf6d8cf7482",
+    "url": "/static/js/3.98bea7d5.chunk.js"
+  },
+  {
+    "revision": "39b4fd00a6e6faf6316a",
+    "url": "/static/js/4.04cf384f.chunk.js"
+  },
+  {
+    "revision": "bf2648b2679a76bb0bc9",
+    "url": "/static/js/5.c9cdd511.chunk.js"
+  },
+  {
+    "revision": "da917afb8204780ca2f9",
+    "url": "/static/js/6.cbb712ac.chunk.js"
+  },
+  {
+    "revision": "33438d935309003dd8cd",
+    "url": "/static/js/7.f0702024.chunk.js"
+  },
+  {
+    "revision": "c5f0809fda00382efc34",
+    "url": "/static/js/8.18c2cf69.chunk.js"
+  },
+  {
+    "revision": "f2c9bc735e01f48cd2f9",
+    "url": "/static/js/9.4961d840.chunk.js"
+  },
+  {
+    "revision": "70942ef21a8b7dbb8fdd",
+    "url": "/static/js/main.1555c2aa.chunk.js"
+  },
+  {
+    "revision": "ce44083c2b79164c8930",
+    "url": "/static/js/runtime-main.8ed8b74d.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "bffe4871e561bc6d63a459e0660b26b7",
+    "url": "/static/media/shopping-bag.bffe4871.svg"
+  }
+]);
